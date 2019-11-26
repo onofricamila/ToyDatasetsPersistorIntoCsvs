e4c565bc96413275ef6e84380e94303215cace09
Camila Onofri <onofricamila2@gmail.com>
2019-11-26 15:33:00 -0300

    total refactoring: config py file added

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
-resourcesFolder = "./resources/"
+import json
+
+csvDatasetsPath = None
+
+def fetchConfig():
+    # we use the global key word to being able to change the values of the variables declared outside the function
+    global csvDatasetsPath
+
+    configFilePath = "/home/camila/Desktop/TESIS/DATA/config.json"
+    with open(configFilePath) as f:
+        data = json.load(f)
+    # fill variables
+    csvDatasetsPath = data.get("csvDatasetsPath")
+
+
+def getClusteringResultsPath():
+    if csvDatasetsPath is not None:
+        return csvDatasetsPath
+    # else
+    fetchConfig()
+    return csvDatasetsPath
+
+resourcesFolder = getClusteringResultsPath()
