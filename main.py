65f2c8c6196679bba9691afc606613f3b117d925
Camila Onofri <onofricamila2@gmail.com>
2019-12-03 15:30:49 -0300

    datasets separated according if they contain time series data or not; specific getters added in config file

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from utils.csvsGenerator import csvsGenerator
 from data.sklearnDatasets import no_structure, blobs, aniso, varied, noisy_moons, noisy_circles
 from data.customCircumferencesDatasetGenerator import custom_circumferences
-from config import resourcesFolder
+from config import resourcesFolder, getNonTimeSeriesDatasetsPath, getTimeSeriesDatasetsPath
 from utils.ndarraysFormCsvsGenerator import ndarraysFormCsvsGenerator
 from utils.datasetDrawer import plotDatasets
 
 # save all the generated data sets together
-datasets = [
+non_time_series_datasets = [
     ({'k': '2', 'name': 'noisy_circles'}, noisy_circles),
     ({'k': '2', 'name': 'noisy_moons'}, noisy_moons),
     ({'k': '3', 'name': 'varied'}, varied),
     ({'k': '3', 'name': 'aniso'}, aniso),
     ({'k': '3', 'name': 'blobs'}, blobs),
     ({'k': '2', 'name': 'no_structure'}, no_structure),
-    ({'k': '2', 'name': 'custom_circumferences'}, custom_circumferences)
     ] # think about what k would be proper for no_structure
 
-datasets = sorted(datasets, key=lambda x: x[0]['name'])
-
+non_time_series_datasets = sorted(non_time_series_datasets, key=lambda x: x[0]['name'])
+time_series_datasets = [
+     ({'k': '2', 'name': 'custom_circumferences'}, custom_circumferences)
+    ]
 # print the generated data sets
-plotDatasets(datasets)
+plotDatasets(non_time_series_datasets + time_series_datasets)
 # store the generated datasets into csv files
-csvsGenerator(datasets, resourcesFolder)
+csvsGenerator(non_time_series_datasets, getNonTimeSeriesDatasetsPath())
+csvsGenerator(time_series_datasets, getTimeSeriesDatasetsPath())
 # obtain the data sets from the csv files
-datasetsFormCsvFiles = ndarraysFormCsvsGenerator(resourcesFolder)
+non_time_series_datasets = ndarraysFormCsvsGenerator(getNonTimeSeriesDatasetsPath())
+time_series_datasets = ndarraysFormCsvsGenerator(getTimeSeriesDatasetsPath())
 # print the fetched data sets
-plotDatasets(datasetsFormCsvFiles)
+plotDatasets(non_time_series_datasets + time_series_datasets)
