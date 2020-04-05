9404ae0245cd81c091aa819d256f87d0cc44d286
Camila Onofri <onofricamila2@gmail.com>
2020-04-04 23:17:51 -0300

    data frame taxis sorted by timestamp

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import json
 
 paths = None
+realDatasetName = None
 
 def _getPaths():
     return paths
@@ -9,11 +10,14 @@ def _getPaths():
 def _fetchConfig():
     # we use the global key word to be able to change the values of the variables declared outside the function
     global paths
+    global realDatasetName
+
     configFilePath = "/home/camila/Desktop/TESIS/DATA/config.json"
     with open(configFilePath) as f:
         data = json.load(f)
     # fill variables
     paths = data.get("paths")
+    realDatasetName = data.get("realDatasetName")
 
 
 def _fetchElementIfNull(_getter):
@@ -30,12 +34,22 @@ def _getElementFromDict(key, _getter):
     return dict.get(key)
 
 
+def _getRealDatasetName():
+    return realDatasetName
+
+
 def getNonTimeSeriesDatasetsPath():
     return _getElementFromDict(key="nonTimeSeriesDatasetsPath", _getter=_getPaths)
 
 
 def getTimeSeriesDatasetsPath():
     return _getElementFromDict(key="timeSeriesDatasetsPath", _getter=_getPaths)
+
+
+def getRealToyDatasetName():
+    return _fetchElementIfNull(_getRealDatasetName)
+
+
 
 
 
--- data/taxis.py
+++ data/taxis.py
@@ -1,13 +1,14 @@
-from config import getTimeSeriesDatasetsPath
+from config import getTimeSeriesDatasetsPath, getRealToyDatasetName
 from utils.ndarraysFormCsvsGenerator import ndarraysFormCsvsGenerator
 import pandas as pd
 
 def prepareRealDataset():
     originalDatasetFile = "/home/camila/Desktop/TESIS/Datasets/Trayectorias - Taxis/dataset/Californa1.csv"
     targetFolder = getTimeSeriesDatasetsPath()
-    targetFileName = "taxis.csv"
+    targetFileName = getRealToyDatasetName() + ".csv"
 
     dataFrame = pd.read_csv(originalDatasetFile, sep=';')
-    # Preview the first 5 lines of the loaded data
-    resultantDataframe = dataFrame[["latitude","longitude"]]
+    dataFrame['to_timestamp'] = dataFrame['to_timestamp'].astype('datetime64[ns]')
+    dataFrameSortedByTimestamp = dataFrame.sort_values(by="to_timestamp")
+    resultantDataframe = dataFrameSortedByTimestamp[["latitude","longitude"]]
     resultantDataframe.to_csv(targetFolder+targetFileName, header=False, index=False)
